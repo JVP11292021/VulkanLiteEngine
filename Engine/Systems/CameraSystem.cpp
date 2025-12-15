1ab29b5fc23136b12205197ce5cc3bb89b68676e
JVP11292021 <jessyvanpolanen@gmail.com>
2025-12-15 09:47:44 +0100

    update: added camera system

---
+++ Engine/Systems/CameraSystem.cpp
@@ -0,0 +1,221 @@
+#include "CameraSystem.hpp"
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <corecrt_math_defines.h>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/string_cast.hpp>
+
+VLE_SYS_NS_B
+
+CameraSystem::CameraSystem(const glm::vec3& position,
+    const glm::vec3& world_up,
+    float yaw,
+    float pitch
+)
+    : position_(position),
+    origin_(position),
+    world_up_(world_up),
+    yaw_(yaw),
+    pitch_(pitch),
+    front_(glm::vec3(1.0f, 0.0f, 0.0f)),
+    movement_speed_(kSpeed),
+    mouse_sensitivity_(kSensitivity),
+    zoom_(kZoom)
+{
+    this->updateCameraVectors();
+}
+
+glm::mat4 CameraSystem::getViewMatrix() const {
+    return glm::lookAt(position_, position_ + front_, up_);
+}
+
+glm::mat4 CameraSystem::getProjMatrix() const {
+    float image_width = image_width_;
+    float image_height = image_height_;
+
+    glm::mat4 persp;
+    persp[0] = glm::vec4(fx_ / image_width, 0.0f, 0.0f, 0.0f);
+    persp[1] = glm::vec4(0.0f, fy_ / image_height, 0.0f, 0.0f);
+    persp[2] = glm::vec4(-(1 - cx_ / image_width), -cy_ / image_height, near_ + far_, -1.0f);
+    persp[3] = glm::vec4(0.0f, 0.0f, near_ * far_, 0.0f);
+
+    glm::mat4 ortho;
+    ortho[0] = glm::vec4(2.0f, 0.0f, 0.0f, 0.0f);
+    ortho[1] = glm::vec4(0.0f, 2.0f, 0.0f, 0.0f);
+    ortho[2] = glm::vec4(0.0f, 0.0f, -2.0f / (far_ - near_), 0.0f);
+    ortho[3] = glm::vec4(-1.0f, -1.0f, -(far_ + near_) / (far_ - near_), 1.0f);
+
+    glm::mat4 proj_full = ortho * persp;
+
+    return proj_full;
+
+}
+
+float CameraSystem::getZoom() const {
+    return zoom_;
+}
+
+void CameraSystem::processKeyboard(CameraSystemMovement CameraSystem_movement, float delta_time) {
+    float delta = movement_speed_ * delta_time;
+    if (CameraSystem_movement == FORWARD) {
+        position_ += front_ * delta;
+    }
+    else if (CameraSystem_movement == BACKWARD) {
+        position_ -= front_ * delta;
+    }
+    else if (CameraSystem_movement == LEFT) {
+        position_ -= right_ * delta;
+    }
+    else if (CameraSystem_movement == RIGHT) {
+        position_ += right_ * delta;
+    }
+    else if (CameraSystem_movement == MOVE_ORIGIN) {
+        position_ = origin_;
+    }
+    else if (CameraSystem_movement == MOVE_TOP) {
+        position_ = glm::vec3(0.0f, 0.0f, 15.0f) * scale_;
+        this->setDirection(glm::vec3(0.0f) * scale_);
+        this->updateCameraVectors();
+    }
+    else if (CameraSystem_movement == MOVE_SIDEWAYS_RIGHT) {
+        position_ = glm::vec3(-5.0f, -5.0f, 5.0f) * scale_;
+        this->setDirection(glm::vec3(0.0f) * scale_);
+        this->updateCameraVectors();
+    }
+    else if (CameraSystem_movement == MOVE_SIDEWAYS_LEFT) {
+        position_ = glm::vec3(-5.0f, 5.0f, 5.0f) * scale_;
+        this->setDirection(glm::vec3(0.0f) * scale_);
+        this->updateCameraVectors();
+    }
+    // std::cout << "CameraSystem: p, y = " << pitch_ << ", " << yaw_ << std::endl;
+    // std::cout << "CameraSystem: position_ = " << glm::to_string(position_) << std::endl;
+}
+
+void CameraSystem::processMouseInput(float xoffset, float yoffset,
+    bool constrain_pitch) {
+    yaw_ -= mouse_sensitivity_ * xoffset;
+    pitch_ += mouse_sensitivity_ * yoffset;
+    this->updateCameraVectors(constrain_pitch);
+}
+
+void CameraSystem::processMouseScroll(float yoffset) {
+    zoom_ -= yoffset;
+    if (zoom_ < 1.0f) {
+        zoom_ = 1.0f;
+    }
+    else if (zoom_ > 45.0f) {
+        zoom_ = 45.0f;
+    }
+    // std::cout << "CameraSystem: zoom = " << zoom_ << std::endl;
+}
+
+void CameraSystem::updateCameraVectors(bool constrain_pitch) {
+    if (constrain_pitch) {
+        if (pitch_ > 89.9f) {
+            pitch_ = 89.9f;
+        }
+        else if (pitch_ < -89.9f) {
+            pitch_ = -89.9f;
+        }
+    }
+
+    glm::vec3 front;
+
+    // front.x = cos(glm::radians(pitch_)) * cos(glm::radians(yaw_));
+    // front.y = sin(glm::radians(pitch_));
+    // front.z = - cos(glm::radians(pitch_)) * sin(glm::radians(yaw_));
+    // glm::vec3 front_orig;
+
+    front.x = cos(glm::radians(pitch_)) * cos(glm::radians(yaw_));
+    front.y = cos(glm::radians(pitch_)) * sin(glm::radians(yaw_));
+    front.z = sin(glm::radians(pitch_));
+
+
+
+    front_ = glm::normalize(front);
+
+    right_ = glm::normalize(glm::cross(front_, world_up_));
+    up_ = glm::normalize(glm::cross(right_, front_));
+
+    // std::cout << "CameraSystem: front_ = " << glm::to_string(front_) << std::endl;
+    // std::cout << "CameraSystem: p, y = " << pitch_ << ", " << yaw_ << std::endl;
+    // std::cout << "CameraSystem: position_ = " << glm::to_string(position_) << std::endl;
+
+}
+
+void CameraSystem::setDirection(const glm::vec3& direction_to) {
+    glm::vec3 direction = glm::normalize(direction_to - position_);
+
+    pitch_ = glm::degrees(asin(direction.z));
+    yaw_ = glm::degrees(atan2(direction.y, direction.x));
+    updateCameraVectors();
+}
+
+void CameraSystem::setPosition(const glm::vec3& position) {
+    position_ = position;
+}
+
+void CameraSystem::setOrigin(const glm::vec3& origin) {
+    origin_ = origin;
+    this->setPosition(origin_);
+}
+
+
+void CameraSystem::setRotation(const float x_angle, const float y_angle, const float z_angle) {
+    pitch_ = glm::degrees(y_angle /* + M_PI_2 */);
+    yaw_ = glm::degrees(z_angle + M_PI_2);
+    updateCameraVectors();
+
+    // glm::mat4 rotation(1.0f);
+    // rotation = glm::rotate(rotation, z_angle, glm::vec3(0.0f, 0.0f, 1.0f));
+    // rotation = glm::rotate(rotation, static_cast<float>(M_PI_2), glm::vec3(0.0f, 1.0f, 0.0f));
+}
+
+void CameraSystem::setRotationPitchYaw(const float pitch_deg, const float yaw_deg) {
+    pitch_ = pitch_deg;
+    yaw_ = yaw_deg;
+    updateCameraVectors();
+}
+
+void CameraSystem::setScale(const float scale) {
+    scale_ = scale;
+}
+
+void CameraSystem::setIntrinsics(
+    const float fx, const float fy, const float cx,
+    const float cy, const float wr = 1.0f
+) {
+    fx_ = fx;
+    fy_ = fy;
+    cx_ = cx;
+    cy_ = cy;
+    wr_ = wr;
+}
+
+void CameraSystem::setIntrinsics(const CameraSystemIntrinsics& CameraSystem_intr) {
+    this->setIntrinsics(CameraSystem_intr.fx, CameraSystem_intr.fy, CameraSystem_intr.cx, CameraSystem_intr.cy, CameraSystem_intr.wr);
+}
+
+void CameraSystem::print(std::ostream& os) const {
+    std::cout << "CameraSystem: p, y = " << pitch_ << ", " << yaw_ << std::endl;
+    std::cout << "CameraSystem: position_ = " << glm::to_string(position_) << std::endl;
+}
+
+CameraSystemIntrinsics CameraSystem::getCameraIntrinsics() const {
+    CameraSystemIntrinsics intr;
+    intr.fx = fx_ / image_width_;
+    intr.fy = fy_ / image_height_;
+    intr.s = 0.0f;
+    intr.cx = cx_ / image_width_;
+    intr.cy = cy_ / image_height_;
+    intr.wr = wr_;
+    return intr;
+}
+
+std::ostream& operator<<(std::ostream& os, const CameraSystemIntrinsics& intr) {
+    intr.print(os);
+    return os;
+}
+
+VLE_SYS_NS_E
---
+++ Engine/Systems/CameraSystem.hpp
@@ -0,0 +1,143 @@
+#ifndef SFM_CAMERA_SYSTEM_H
+#define SFM_CAMERA_SYSTEM_H
+
+#include "engdefs.hpp"
+#include <iostream>
+#include <glm/glm.hpp>
+
+VLE_SYS_NS_B
+
+enum CameraSystemMovement
+{
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT,
+
+    MOVE_ORIGIN,
+    MOVE_TOP,
+    MOVE_SIDEWAYS_RIGHT,
+    MOVE_SIDEWAYS_LEFT
+};
+
+struct CameraSystemIntrinsics {
+    float fx;
+    float fy;
+    float s;
+    float cx;
+    float cy;
+    float wr;
+    glm::mat3 getCameraMatrix() const {
+        glm::mat3 camera_matrix(1.0);
+        camera_matrix[0] = { fx /* *wr */, 0.0, 0.0 };
+        camera_matrix[1] = { 0.0, fy, 0.0 };
+        camera_matrix[2] = { cx /* *wr*/, cy, 1.0 };
+        return camera_matrix;
+    }
+    void print(std::ostream& os = std::cout) const {
+        os << "(fx = " << fx << ", ";
+        os << "fy = " << fy << ", ";
+        os << "cx = " << cx << ", ";
+        os << "cy = " << cy << ", ";
+        os << "wr = " << wr << ")";
+    }
+};
+
+std::ostream& operator<<(std::ostream& os, const CameraSystemIntrinsics& intr);
+
+// camera constant
+const float kYaw = 0.0f;
+const float kPitch = 0.0f;  // -5.0f    5.0f
+const float kSpeed = 60.2f; // 2.5f 60.5f
+const float kSensitivity = 0.1f;
+const float kZoom = 45.0f;
+
+class CameraSystem {
+public:
+    CameraSystem(const glm::vec3& position = glm::vec3(0.0f, 0.0f, 0.0f),
+        const glm::vec3& world_up = glm::vec3(0.0f, 0.0f, 1.0f),
+        float yaw = kYaw, float pitch = kPitch);
+
+    CameraSystem(const CameraSystem&) = delete;
+    CameraSystem& operator=(const CameraSystem&) = delete;
+
+public:
+    void processKeyboard(CameraSystemMovement camera_movement, float delta_time);
+    void processMouseInput(float xoffset, float yoffset, bool constrain_pitch = true);
+    void processMouseScroll(float yoffset);
+
+public:
+    glm::mat4 getViewMatrix() const;
+    glm::mat4 getProjMatrix() const;
+    float getZoom() const;
+    inline glm::vec3 getPosition() { return position_; };
+    inline float getRotationPitch() { return pitch_; };
+    inline float getRotationYaw() { return yaw_; };
+
+    void setDirection(const glm::vec3& direction_to);
+    void setPosition(const glm::vec3& position);
+    void setOrigin(const glm::vec3& origin);
+    void setRotation(const float x_angle, const float y_angle, const float z_angle);
+    void setRotationPitchYaw(const float pitch, const float yaw);
+    void setScale(const float scale);
+
+    void print(std::ostream& os = std::cout) const;
+
+    void setIntrinsics(const float fx, const float fy, const float cx,
+        const float cy, const float wr);
+
+    void setIntrinsics(const CameraSystemIntrinsics& camera_intr);
+
+    inline float getImageWidth() { return image_width_; }
+    inline float getImageHeight() { return image_height_; }
+
+    inline float getCx() const { return cx_; }
+    inline float getCy() const { return cy_; }
+    inline float getFx() const { return fx_; }
+    inline float getFy() const { return fy_; }
+
+    CameraSystemIntrinsics getCameraIntrinsics() const;
+
+private:
+    void updateCameraVectors(bool constrain_pitch = true);
+
+private:
+
+    // Intrinsics
+    float fx_ = 1450.317230113;
+    float fy_ = 1451.184836113;
+    float cx_ = 1244.386581025;
+    float cy_ = 1013.145997723;
+
+    float image_width_ = 2452;
+    float image_height_ = 2056;
+
+    float wr_ = 2452.0f / 2056.0f;
+
+
+    float near_ = 0.1;
+    float far_ = 5000;
+
+    float scale_ = 1.0f;
+
+    // Extrinsics
+    glm::vec3 position_;
+    glm::vec3 front_;
+    glm::vec3 up_;
+    glm::vec3 right_;
+    glm::vec3 world_up_;
+
+    float yaw_;
+    float pitch_;
+
+    float movement_speed_;
+    float mouse_sensitivity_;
+
+    glm::vec3 origin_;
+
+    float zoom_;
+};
+
+VLE_SYS_NS_E
+
+#endif // SFM_CAMERA_SYSTEM_H
