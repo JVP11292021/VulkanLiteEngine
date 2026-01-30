c87c5cc10f29450e8814583803a94e7756b56da9
Rolan <107186461+rolanvb@users.noreply.github.com>
2026-01-30 10:53:04 +0100

    Update Renderer.cpp

--- Engine/Systems/Renderer.cpp
+++ Engine/Systems/Renderer.cpp
@@ -79,7 +79,7 @@ void Renderer::beginSwapChainRenderPass(VkCommandBuffer commandBuffer) {
 	renderPassInfo.renderArea.extent = this->swapChain->getSwapChainExtent();
 
 	std::array<VkClearValue, 2> clearValues = {};
-	clearValues[0].color = { 0.1f, 0.1f, 0.1f, 1.0f };
+	clearValues[0].color = { 0.3f, 0.3f, 0.3f, 1.0f };
 	clearValues[1].depthStencil = { 1.0f, 0 };
 	renderPassInfo.clearValueCount = static_cast<std::uint32_t>(clearValues.size());
 	renderPassInfo.pClearValues = clearValues.data();
